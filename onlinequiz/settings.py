41719abdef6e4d3f03aeff8f517b535cf67b59b3
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-10-31 06:49:46 +0000

    Update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -81,10 +81,11 @@ WSGI_APPLICATION = 'onlinequiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / "db.sqlite3",  # This works correctly
     }
 }
 
