631b0023f13f971dded17c46dd8848504a468b57
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-12-26 21:57:48 +0000

    Update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'onlinequiz.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-''''
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+"""
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
