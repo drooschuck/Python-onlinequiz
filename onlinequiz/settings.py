203a329651b5a64fb7115de289cfe632f4eddc45
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-12-26 04:55:52 +0000

    Update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ks3-jml1.onrender.com/]
+ALLOWED_HOSTS = [ks3-jml1.onrender.com]
 
 
 # Application definition
