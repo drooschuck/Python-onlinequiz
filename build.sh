c98395fbe446a544c6a1ff7ae64818ae97ba868d
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-12-27 01:51:35 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Exit on error
-set -o errexit
+#set -o errexit
 
 # Install dependencies
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # Collect static files
-#python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 
 # Apply database migrations
-python manage.py migrate
+# python manage.py migrate
